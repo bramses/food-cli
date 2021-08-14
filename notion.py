ed4baf26a95d5c2fcdd0d2312775f819a0991c43
Bram Adams <3282661+bramses@users.noreply.github.com>
2021-08-14 18:29:01 -0400

    notion

--- notion.py
+++ notion.py
@@ -20,6 +20,20 @@ load_dotenv(dotenv_path=env_path)
 async def create_food(food):
     return await create_page(build_page_properties_from_food(food), os.getenv('NOTION_DATABASE_ID'))
 
+async def on_request_start(
+        session, trace_config_ctx, params):
+    print("Starting request")
+    print(session, trace_config_ctx, params)
+    
+
+async def on_request_end(session, trace_config_ctx, params):
+    print("Ending request")
+    print(session, trace_config_ctx, params)
+
+trace_config = aiohttp.TraceConfig()
+trace_config.on_request_start.append(on_request_start)
+trace_config.on_request_end.append(on_request_end)
+
 async def create_page(properties, database_id=os.getenv('NOTION_DATABASE_ID')):
     root_url = 'https://api.notion.com/v1/pages'
 
@@ -36,9 +50,10 @@ async def create_page(properties, database_id=os.getenv('NOTION_DATABASE_ID')):
 
     print(json.dumps(data, indent=4))
 
-    async with aiohttp.ClientSession() as session:
-        async with session.post(root_url, headers=headers, data=data) as resp:
+    async with aiohttp.ClientSession(trace_configs=[trace_config]) as session:
+        async with session.post(root_url, headers=headers, json=data) as resp:
             jsonRes = await resp.json()
+            print(json.dumps(jsonRes, indent=4))
             if jsonRes['object'] == 'error':
                 raise Exception(jsonRes['message'])
             return jsonRes
@@ -85,19 +100,19 @@ async def query_database_for_food_names(food_names):
 def build_page_properties_from_food(food = dict()):
     properties = dict()
     properties['Food'] = set_property_value(food.get('name', ''), 'title', 'Food')
-    properties['Brand'] = set_property_value(food.get('brand', ''), 'rich_text', 'Brand')
+    # properties['Brand'] = set_property_value(food.get('brand', ''), 'rich_text', 'Brand')
     properties['Meal'] = set_property_value(food.get('meal', ''), 'select', 'Meal')
     properties['Calories'] = set_property_value(food.get('calories', -1), 'number', 'Calories')
     properties['Fat'] = set_property_value(food.get('fat', -1), 'number', 'Fat')
-    properties['Saturated_Fat'] = set_property_value(food.get('saturated_fat', -1), 'number', 'Saturated_Fat')
+    properties['Saturated_Fat'] = set_property_value(food.get('saturated_fat', -1), 'number', 'Saturated Fat')
     properties['Carbohydrates'] = set_property_value(food.get('carbohydrates', -1), 'number', 'Carbohydrates')
     properties['Sugar'] = set_property_value(food.get('sugar', -1), 'number', 'Sugar')
     properties['Protein'] = set_property_value(food.get('protein', -1), 'number', 'Protein')
     properties['Sodium'] = set_property_value(food.get('sodium', -1), 'number', 'Sodium')
     properties['Fiber'] = set_property_value(food.get('fiber', -1), 'number', 'Fiber')
-    properties['Has_Processed_Sugar'] = set_property_value(food.get('has_processed_sugar', False), 'checkbox', 'Has_Processed_Sugar')
-    properties['Has_Dairy'] = set_property_value(food.get('has_dairy', False), 'checkbox', 'Has_Dairy')
-    properties['Favorite'] = set_property_value(food.get('favorite', False), 'checkbox', 'Favorite')
+    # properties['Has_Processed_Sugar'] = set_property_value(food.get('has_processed_sugar', False), 'checkbox', 'Has_Processed_Sugar')
+    # properties['Has_Dairy'] = set_property_value(food.get('has_dairy', False), 'checkbox', 'Has_Dairy')
+    # properties['Favorite'] = set_property_value(food.get('favorite', False), 'checkbox', 'Favorite')
     return properties
 
 '''
@@ -133,28 +148,31 @@ def generate_random_short_string():
 def set_property_value(property_name, property_value, validation_name=None):
     if property_value == 'title':
         try:
-           property_value = {'title' : [{'text': {'content': property_name}}], 'name': validation_name }
+           property_value = {'title' : [{'text': {'content': property_name}}], 'name': validation_name, 'type': 'title', 'id': generate_random_short_string() }
            return property_value
         except:
-            return {'title' : [{'text': {'content': ''}}], 'name': validation_name }
+            return {'title' : [{'text': {'content': ''}}], 'name': validation_name, 'type': 'title' }
     elif property_value == 'rich_text':
+        return None
         try:
-            property_value = {'rich_text': [{'text': {'content': property_name}}], 'name': validation_name }
+            property_value = {'rich_text': [{'text': {'content': property_name}}], 'name': validation_name, 'type': 'text' }
             return property_value
         except:
-            return {'rich_text': [{'text': {'content': ''}}], 'name': validation_name }
+            return {'rich_text': [{'text': {'content': ''}}], 'name': validation_name, 'type': 'text' }
     elif property_value == 'number':
+        return None
         try:
-            property_value = {'number': property_name, 'name': validation_name }
+            property_value = {'number': property_name, 'name': validation_name, 'type': 'number' }
             return property_value
         except:
-            return {'number': -1, 'name': validation_name }
+            return {'number': -1, 'name': validation_name, 'type': 'number' }
     elif property_value == 'checkbox':
+        return None
         try:
-            property_value = {'checkbox': property_name, 'name': validation_name }
+            property_value = {'checkbox': property_name, 'name': validation_name, 'type': 'checkbox' }
             return property_value
         except:
-            return {'checkbox': False, 'name': validation_name }
+            return {'checkbox': False, 'name': validation_name, 'type': 'checkbox' }
     else:
         return None
 
